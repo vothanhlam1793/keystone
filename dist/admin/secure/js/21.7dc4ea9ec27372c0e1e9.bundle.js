8457ce254cc20bab0a2b7f702e20d5a5c1ac59ee
Vo Thanh Lam <vothanhlam1793@gmail.com>
2022-01-18 11:22:36 +0700

    keystone 1

--- dist/admin/secure/js/21.7dc4ea9ec27372c0e1e9.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{507:function(a,t,n){"use strict";n.r(t);var e=n(508),c=n(510);t.default=a=>{if(!a.data)return null;const t=a.field.config.format;return t?Object(e.a)(Object(c.a)(a.data),t):a.data}}}]);
